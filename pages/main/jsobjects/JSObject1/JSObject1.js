1aafdae6423c87d4a265ed89db1e406d5228496f
chan park <nicestcurry@gmail.com>
2025-08-02 05:58:31 +0000

    노티 page 작업 - Fridge Item 만료, 임박 상품 검색 - 서버 시간 추가

--- pages/main/jsobjects/JSObject1/JSObject1.js
+++ pages/main/jsobjects/JSObject1/JSObject1.js
@@ -1,12 +1,32 @@
 export default {
 
+	state: {
+		server_time: "None"
+	},
+
+
+
 	async ping() {
 		try {
+
+			await this.get_servertime();
 			const response = await PING.run();
 			showAlert(`Success: ${response.message}`); // 결과 보여주기
+
+
 		} 
 		catch (error) {
 			showAlert(`에러: ${error}`);
 		}
-	}
+	},
+
+	async get_servertime() {
+		try {
+			const response = await GET_time.run();
+			this.state.server_time =response.time; 
+		} 
+		catch (error) {
+			showAlert(`에러: ${error}`);
+		}
+	},
 }
